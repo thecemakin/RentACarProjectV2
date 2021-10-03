98271d84a154b1419fc92454f82c2327579a1871
CEM AKIN <20184029027@std.nisantasi.edu.tr>
2021-10-03 16:34:34 +0300

    update

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -17,29 +19,36 @@ namespace Business.Concrete
             _colorDal = colorDal;
         }
 
-        public void Add(Color color)
+        public IResult Add(Color color)
         {
             _colorDal.Add(color);
+            return new SuccessDataResult<Color>(Messages.ColorAdded);
         }
 
-        public void Delete(Color color)
+        public IResult Delete(Color color)
         {
             _colorDal.Delete(color);
+            return new SuccessDataResult<Color>(Messages.ColorDeleted);
         }
 
-        public List<Color> GetAll()
+        public IDataResult<List<Color>> GetAll()
         {
-            return _colorDal.GetAll();
+            return new SuccessDataResult<List<Color>> ( _colorDal.GetAll());
         }
 
-        public Color GetById(int id)
+        public IDataResult<Color> GetById(int id)
         {
-            return _colorDal.Get(c=>c.ColorId == id);
+            return new SuccessDataResult<Color> (_colorDal.Get(c=>c.ColorId == id));
         }
 
         public void Update(Color color)
         {
             _colorDal.Update(color);
+        }
+
+        IResult IColorService.Update(Color color)
+        {
+            return new SuccessDataResult<Color>(Messages.ColorUpdated);
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,7 +10,20 @@ namespace Business.Constants
         public static string CarNameInvalid = "Araç ismi geçersiz.";
         public static string CarDeleted = "Araç Silindi.";
         public static string CarUpdated = "Araç güncellendi.";
+
+        public static string BrandAdded = "Marka eklendi.";
+        public static string BrandNameInvalid = "Marka ismi geçersiz.";
+        public static string BrandDeleted = "Marka silindi.";
+        public static string BrandUpdated = "Araç güncellendi.";
+
+        public static string ColorAdded = "Renk eklendi.";
+        public static string ColorNameInvalid = "Renk ismi geçersiz";
+        public static string ColorDeleted = "Renk silindi.";
+        public static string ColorUpdated = "Renk güncellendi.";
+
         internal static string MaintenanceTime ;
         internal static string CarListed ;
+        internal static string BrandListed;
+        internal static string ColorListed;
     }
 }
